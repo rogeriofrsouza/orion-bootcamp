a7a9f6c7a9b4b468ac62911d89bd9ee0b1297ac3
Rogerio Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-10-11 16:04:50 -0300

    Ex1: Ajustes na função contaVogais, comentários e tipagem

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,21 +1,33 @@
-var btn = document.querySelector('button');
-var output = document.getElementById('output');
-function qtdeVogais(str) {
-    var vogais = 'aeiou' + 'áéíóú' + 'ãẽĩõũ' + 'âêîôû';
-    var qtde = 0;
-    for (var i = 0; i < str.length; i++) {
-        if (vogais.indexOf(str[i]) > -1) {
-            qtde++;
+// Referências de elementos no DOM
+var botao = document.querySelector('#conta-vogais');
+var saida = document.querySelector('#saida');
+/**
+ * Retorna a quantidade de vogais contidas em uma palavra.
+ *
+ * @param palavra - A palavra a ser percorrida.
+ * @returns O numero de vogais encontradas em palavra.
+ *
+ */
+function contaVogais(palavra) {
+    var vogais = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ã', 'ẽ', 'ĩ', 'õ', 'ũ', 'â', 'ê', 'î', 'ô', 'û'];
+    var quantidade = 0;
+    // Percorre a string
+    for (var _i = 0, palavra_1 = palavra; _i < palavra_1.length; _i++) {
+        var letra = palavra_1[_i];
+        if (vogais.includes(letra.toLowerCase())) {
+            quantidade++;
         }
     }
-    return qtde;
+    return quantidade;
 }
-btn.addEventListener('click', function () {
-    var palavraInput = document.querySelector('input').value;
+// Event handlers
+botao.addEventListener('click', function () {
     var palavra = 'Paralelepípedo';
+    var palavraInput = document.querySelector('#palavra').value;
     var para1 = document.createElement('p');
     var para2 = document.createElement('p');
-    para1.textContent = "A palavra \"".concat(palavra, "\" possui ").concat(qtdeVogais(palavra), " vogais.");
-    para2.textContent = "A palavra \"".concat(palavraInput, "\" possui ").concat(qtdeVogais(palavraInput), " vogais.");
-    output.append(para1, para2);
+    para1.textContent = "A palavra \"".concat(palavra, "\" possui ").concat(contaVogais(palavra), " vogais.");
+    para2.textContent = "A palavra \"".concat(palavraInput, "\" possui ").concat(contaVogais(palavraInput), " vogais.");
+    saida.innerHTML = '';
+    saida.append(para1, para2);
 });
--- ex1/ex1.ts
+++ ex1/ex1.ts
@@ -1,26 +1,37 @@
-const btn = document.querySelector('button');
-const output = document.getElementById('output');
+// Referências de elementos no DOM
+const botao = document.querySelector('#conta-vogais');
+const saida = document.querySelector('#saida');
 
-function qtdeVogais(str: string): number {
-  const vogais = 'aeiou' + 'áéíóú' + 'ãẽĩõũ' + 'âêîôû';
-  let qtde = 0;
+/**
+ * Retorna a quantidade de vogais contidas em uma palavra.
+ *
+ * @param palavra - A palavra a ser percorrida.
+ * @returns O numero de vogais encontradas em palavra.
+ *
+ */
+function contaVogais(palavra: string): number {
+  const vogais: string[] = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ã', 'ẽ', 'ĩ', 'õ', 'ũ', 'â', 'ê', 'î', 'ô', 'û'];
+  let quantidade: number = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (vogais.indexOf(str[i]) > -1) {
-      qtde++;
+  // Percorre a string
+  for (const letra of palavra) {
+    if (vogais.includes(letra.toLowerCase())) {
+      quantidade++;
     }
   }
 
-  return qtde;
+  return quantidade;
 }
 
-btn!.addEventListener('click', () => {
-  const palavraInput = document.querySelector('input')!.value;
-  const palavra = 'Paralelepípedo';
+// Event handlers
+botao!.addEventListener('click', () => {
+  const palavra: string = 'Paralelepípedo';
+  const palavraInput: string = (document.querySelector('#palavra') as HTMLInputElement).value;
   const para1 = document.createElement('p');
   const para2 = document.createElement('p');
 
-  para1.textContent = `A palavra "${palavra}" possui ${qtdeVogais(palavra)} vogais.`;
-  para2.textContent = `A palavra "${palavraInput}" possui ${qtdeVogais(palavraInput)} vogais.`;
-  output!.append(para1, para2);
+  para1.textContent = `A palavra "${palavra}" possui ${contaVogais(palavra)} vogais.`;
+  para2.textContent = `A palavra "${palavraInput}" possui ${contaVogais(palavraInput)} vogais.`;
+  saida!.innerHTML = '';
+  saida!.append(para1, para2);
 });
