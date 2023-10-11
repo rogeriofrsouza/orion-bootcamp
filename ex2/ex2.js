951962ac8471f7fdb056ececd823ec3a92397810
Rogerio Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-10-11 17:32:19 -0300

    Ex2: Ajustes na lista, buscaPorId

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,21 +1,22 @@
 var lista = [
-    { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
-    { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
-    { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
-    { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+    { id: 1, nome: "Ada Lovelace", bio: "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
+    { id: 2, nome: "Alan Turing", bio: "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
+    { id: 3, nome: "Nikola Tesla", bio: "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
+    { id: 4, nome: "Nicolau Copérnico", bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
 /*
  * Paradigma funcional
  */
-var getBio = function (id, lista) { var _a; return (_a = lista.find(function (p) { return p.id === id; })) === null || _a === void 0 ? void 0 : _a.bio; };
-var getName = function (id, lista) { var _a; return (_a = lista.find(function (p) { return p.id === id; })) === null || _a === void 0 ? void 0 : _a.name; };
-var removePessoa = function (id, lista) { return lista.filter(function (p) { return p.id !== id; }); };
-var alteraPessoa = function (id, lista, name, bio) { return lista.map(function (p) { return (p.id === id) ? { "id": id, "name": name, "bio": bio } : p; }); };
-var imprimeLista = function (lista) { return lista.forEach(function (p) { return console.log("".concat(p.id, ". ").concat(p.name)); }); };
+var buscaPorId = function (id, lista) { return lista.find(function (pessoa) { return pessoa.id === id; }); };
+var acessarBio = function (id, lista, buscaPorId) { var _a; return (_a = buscaPorId(id, lista)) === null || _a === void 0 ? void 0 : _a.bio; };
+var acessarNome = function (id, lista, buscaPorId) { var _a; return (_a = buscaPorId(id, lista)) === null || _a === void 0 ? void 0 : _a.nome; };
+var removePessoa = function (id, lista) { return lista.filter(function (pessoa) { return pessoa.id !== id; }); };
+var alteraPessoa = function (id, lista, nome, bio) { return lista.map(function (pessoa) { return (pessoa.id === id) ? { id: id, nome: nome, bio: bio } : pessoa; }); };
+var imprimeLista = function (lista) { return lista.forEach(function (pessoa) { return console.log("".concat(pessoa.id, ". ").concat(pessoa.nome)); }); };
 console.log('1. Demonstracao funcional');
 console.log('-------------------------');
-console.log("a) ".concat(getBio(2, lista)));
-console.log("b) ".concat(getName(2, lista)));
+console.log("a) ".concat(acessarBio(2, lista, buscaPorId)));
+console.log("b) ".concat(acessarNome(2, lista, buscaPorId)));
 var aux = removePessoa(4, lista);
 console.log("\nc) Pessoa 4 removida. Lista atualizada:");
 imprimeLista(aux);
@@ -25,29 +26,29 @@ imprimeLista(aux);
 /*
  * Paradigma imperativo
  */
-function getBioImp(id) {
+function acessarBioImp(id) {
     return lista[id - 1].bio;
 }
-function getNameImp(id) {
-    return lista[id - 1].name;
+function acessarNomeImp(id) {
+    return lista[id - 1].nome;
 }
 function removePessoaImp(id) {
     lista.splice(id - 1, 1);
 }
-function alteraPessoaImp(id, name, bio) {
-    lista[id - 1].name = name;
+function alteraPessoaImp(id, nome, bio) {
+    lista[id - 1].nome = nome;
     lista[id - 1].bio = bio;
 }
 function imprimeListaImp() {
     for (var _i = 0, lista_1 = lista; _i < lista_1.length; _i++) {
         var pessoa = lista_1[_i];
-        console.log("".concat(pessoa["id"], ". ").concat(pessoa["name"]));
+        console.log("".concat(pessoa.id, ". ").concat(pessoa.nome));
     }
 }
 console.log('\n2. Demonstracao imperativo');
-console.log('-------------------------');
-console.log("a) ".concat(getBioImp(3)));
-console.log("b) ".concat(getNameImp(3)));
+console.log('--------------------------');
+console.log("a) ".concat(acessarBioImp(3)));
+console.log("b) ".concat(acessarNomeImp(3)));
 removePessoaImp(2);
 console.log("\nc) Pessoa 2 removida. Lista atualizada:");
 imprimeListaImp();
--- ex2/ex2.ts
+++ ex2/ex2.ts
@@ -1,36 +1,38 @@
 interface Pessoa {
   id: number;
-  name: string;
+  nome: string;
   bio: string;
 }
 
 const lista: Pessoa[] = [
-  { "id": 1, "name": "Ada Lovelace", "bio": "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
-  { "id": 2, "name": "Alan Turing", "bio": "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
-  { "id": 3, "name": "Nikola Tesla", "bio": "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
-  { "id": 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
+  { id: 1, nome: "Ada Lovelace", bio: "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina" },
+  { id: 2, nome: "Alan Turing", bio: "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial" },
+  { id: 3, nome: "Nikola Tesla", bio: "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada." },
+  { id: 4, nome: "Nicolau Copérnico", bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
 
 /* 
  * Paradigma funcional
  */
 
-const getBio = (id: number, lista: Pessoa[]) => lista.find(p => p.id === id)?.bio;
+const buscaPorId = (id: number, lista: Pessoa[]) => lista.find((pessoa: Pessoa) => pessoa.id === id);
 
-const getName = (id: number, lista: Pessoa[]) => lista.find(p => p.id === id)?.name;
+const acessarBio = (id: number, lista: Pessoa[], buscaPorId: Function) => buscaPorId(id, lista)?.bio;
 
-const removePessoa = (id: number, lista: Pessoa[]) => lista.filter(p => p.id !== id);
+const acessarNome = (id: number, lista: Pessoa[], buscaPorId: Function) => buscaPorId(id, lista)?.nome;
 
-const alteraPessoa = (id: number, lista: Pessoa[], name: string, bio: string) => lista.map(p => (p.id === id) ? { "id": id, "name": name, "bio": bio } : p);
+const removePessoa = (id: number, lista: Pessoa[]) => lista.filter((pessoa: Pessoa) => pessoa.id !== id);
 
-const imprimeLista = (lista: Pessoa[]) => lista.forEach(p => console.log(`${p.id}. ${p.name}`));
+const alteraPessoa = (id: number, lista: Pessoa[], nome: string, bio: string) => lista.map((pessoa: Pessoa) => (pessoa.id === id) ? { id: id, nome: nome, bio: bio } : pessoa);
+
+const imprimeLista = (lista: Pessoa[]) => lista.forEach((pessoa: Pessoa) => console.log(`${pessoa.id}. ${pessoa.nome}`));
 
 console.log('1. Demonstracao funcional');
 console.log('-------------------------');
-console.log(`a) ${getBio(2, lista)}`);
-console.log(`b) ${getName(2, lista)}`);
+console.log(`a) ${acessarBio(2, lista, buscaPorId)}`);
+console.log(`b) ${acessarNome(2, lista, buscaPorId)}`);
 
-let aux = removePessoa(4, lista);
+let aux: Pessoa[] = removePessoa(4, lista);
 console.log(`\nc) Pessoa 4 removida. Lista atualizada:`);
 imprimeLista(aux);
 
@@ -42,33 +44,33 @@ imprimeLista(aux);
  * Paradigma imperativo
  */
 
-function getBioImp(id: number): string {
+function acessarBioImp(id: number): string {
   return lista[id - 1].bio;
 }
 
-function getNameImp(id: number): string {
-  return lista[id - 1].name;
+function acessarNomeImp(id: number): string {
+  return lista[id - 1].nome;
 }
 
 function removePessoaImp(id: number): void {
   lista.splice(id - 1, 1);
 }
 
-function alteraPessoaImp(id: number, name: string, bio: string): void {
-  lista[id - 1].name = name;
+function alteraPessoaImp(id: number, nome: string, bio: string): void {
+  lista[id - 1].nome = nome;
   lista[id - 1].bio = bio;
 }
 
 function imprimeListaImp(): void {
   for (const pessoa of lista) {
-    console.log(`${pessoa["id"]}. ${pessoa["name"]}`);
+    console.log(`${pessoa.id}. ${pessoa.nome}`);
   }
 }
 
 console.log('\n2. Demonstracao imperativo');
-console.log('-------------------------');
-console.log(`a) ${getBioImp(3)}`);
-console.log(`b) ${getNameImp(3)}`);
+console.log('--------------------------');
+console.log(`a) ${acessarBioImp(3)}`);
+console.log(`b) ${acessarNomeImp(3)}`);
 
 removePessoaImp(2);
 console.log(`\nc) Pessoa 2 removida. Lista atualizada:`);
