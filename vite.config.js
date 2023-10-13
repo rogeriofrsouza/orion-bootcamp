01e640ae14b68e73f350461fd20ddd1b028e9e00
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-10-13 16:09:08 -0300

    Configuração no Vite para build de uma aplicação Multi-Page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/orion-bootcamp/'
+  base: '/orion-bootcamp/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ex1: resolve(__dirname, 'src/ex1/ex1.html'),
+        ex2: resolve(__dirname, 'src/ex2/ex2.html')
+      }
+    }
+  }
 });
